d76e312a152329c6f790a97389246690cf63214b
Jacqui Manzi <jacquimanzi@gmail.com>
2016-06-28 22:26:25 -0700

    Colors check in.

---
+++ utils/Pattern.js
@@ -0,0 +1,105 @@
+
+let white = '#ffffff';  // white
+let black = '#000000';  // black
+
+let blues = ['#0000ff', // blue
+			 '#66CCCC', // aqua
+			 '#F0F8FF', // alice blue
+			 '#4E78A0', // aquamarine
+			 '#5f9ea0', // cadet blue
+			 '#6495ed', // cornflower blue
+			 '#00ffff', // cyan
+			 '#00008b', // dark blue
+			 '#008b8b', // dark cyan
+			 '#00CED1', // dark turquoise
+			 '#00BFFF', // deep sky blue
+			 '#1E90FF', // dodger blue
+			 '#0000CD', // medium blue
+			 '#7B68EE', // medium slate blue
+			 '#48D1CC', // medium turquoise
+			 '#000080', // navy
+			 '#4169E1', // royal blue
+			 '#87CEEB', // sky blue
+			 '#4682B4', // steel blue
+			 '#40E0D0' // turquoise
+			 ];
+
+let oranges = ['#d2691e', // chocolate
+			   '#ff7f50', // coral
+			   '#ff8c00', // dark orange
+			   '#E9967A', // dark salmon
+			   '#FFA07A', // light salmon
+			   '#FF4500', // orange red
+			   '#CD853F', // peru
+			   '#8B4513', // saddle brown
+			   '#A0522D', // sienna
+			   '#ff6347' // tomato
+			   ];
+
+let yellows = ['#FFD700', // gold
+			   '#DAA520', // goldenrod
+			   '#B8860B' // dark goldenrod
+			   ];
+
+let greens = ['#00ff00', // green
+			  '#7fff00', // chartreuse
+			  '#006400', // dark green
+			  '#2F4F4F', // dark slate grey
+			  '#228B22', // forest green
+			  '#ADFF2F', // green yellow
+			  '#7CFC00', // lawn green
+			  '#20B2AA', // light sea green
+			  '#66CDAA', // medium aquamarine
+			  '#3CB371', // medium sea green
+			  '#00FA9A', // medium spring green
+			  '#808000', // olive
+			  '#6B8E23', // olive drab
+			  '#2E8B57', // sea green
+			  '#00FF7F', // spring green
+			  '#008080', // teal
+			  '#9ACD32' // yellow green
+			  ];
+
+let reds = ['#ff0000', // red 
+			'#a52a2a', // brown
+			'#dc143c', // crimson
+			'#B22222', // fire brick
+			'#8B0000', // dark red
+			'#CD5C5C', // indian red
+			'#800000' // maroon
+			];       
+
+let pinks = ['#FF1493', // deep pink
+			 '#FF00FF', // fushia
+			 '#FF69B4', // hot pink
+			 '#F08080', // light coral
+			 '#FFB6C1', // light pink
+			 '#FF00FF', // magenta
+			 '#C71585', // medium violet red
+			 '#DA70D6', // orchid
+			 '#DB7093', // pale violet red
+			 '#FFC0CB', // pink
+			 '#DDA0DD', // plum
+			 '#EE82EE' // violet
+			 ];
+
+let purples = ['#8a2be2', // blue violet
+			   '#8B008B', // dark magenta
+			   '#9932cc', // dark orchid
+			   '#483D8B', // dark slate blue
+			   '#9400D3', // dark violet
+			   '#800080', // purple
+			   '#4B0082', // indigo
+			   '#BA55D3', // medium orchid
+			   '#9370DB', // medium purple
+			   '#6A5ACD' // slate blue
+			  ];
+
+
+let allColors = blues
+					.concat(reds)
+					.concat(greens)
+					.concat(yellows)
+					.concat(purples)
+					.concat(oranges)
+					.concat(pinks);
